af0e1dcd54cb1ad0c2cd5f5f465015b5f02a6112
admin <denis.lamochenko@gmail.com>
2025-05-23 22:58:30 +0800

    refactor: animals_filter.py

--- src/animals/animals_filter.py
+++ src/animals/animals_filter.py
@@ -4,9 +4,11 @@ from fastapi_filter.contrib.sqlalchemy import Filter
 from src.models.AnimalModel import Animal
 
 class AnimalsFilter(Filter):
-    name: Optional[str]
-    description: Optional[str] = None
-    population: Optional[int] = None
+    name__ilike: str | None = None
+    description__like: str | None = None
+    population__gte: int | None = None
+    population__lte: int | None = None
+    population__in: list[int] | None = None
 
     class Constants(Filter.Constants):
         model = Animal
